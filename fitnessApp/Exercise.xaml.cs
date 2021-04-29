744375e824491840eb3456db8dda48d83fbc5e5d
owain-mahoney <56028942+owain-mahoney@users.noreply.github.com>
2021-04-29 16:14:29 +0100

    Add files via upload

---
+++ fitnessApp/Exercise.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace fitnessApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Exercise : ContentPage
+    {
+        public Exercise()
+        {
+            InitializeComponent();
+        }
+        public async void onButtonHomeClicked(object sender, EventArgs args)
+        {
+            await Application.Current.MainPage.Navigation.PopToRootAsync();
+
+        }
+        public async void onButtonProfileClicked(object sender, EventArgs args)
+        {
+           
+            await Navigation.PushAsync(new Profile());
+        }
+
+        private async void RunningImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProgressPage(new viewModel.ProgressPageViewModel(9.8)));
+        }
+        private async void WalkingImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProgressPage(new viewModel.ProgressPageViewModel(4.3)));
+        }
+        private async void CyclingImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProgressPage(new viewModel.ProgressPageViewModel(8.0)));
+        }
+    }
+}
---
+++ fitnessApp/models/exceriseDBOps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Xamarin.Essentials;
+using System.Threading.Tasks;
+namespace fitnessApp.models
+ 
+{
+    class exceriseDBOps
+    {
+        private SQLiteAsyncConnection _conncetion;
+        private Databasemodel _dbModel;
+        public exceriseDBOps()
+        {
+            _dbModel = new Databasemodel();
+            _conncetion = _dbModel.GetConnection();
+            _conncetion.CreateTableAsync<Tables.Point>();
+            _conncetion.CreateTableAsync<Tables.Person>();
+           
+        }
+        public async Task<Tables.Person> GetPersonAsync()
+        {
+            return await _conncetion.FindAsync<Tables.Person>(1);
+        }
+        public async Task AddPoint(Tables.Point point)
+        {
+            await _conncetion.InsertAsync(point);
+        }
+        public async Task<Tables.Point>GetLastPoint(int id )
+        {
+            return await _conncetion.FindAsync<Tables.Point>(id);
+        }
+
+        public async Task UpdatePerson(Tables.Person person)
+        {
+            await _conncetion.UpdateAsync(person);
+        }
+        public async Task InsertPerson(Tables.Person person)
+        {
+            await _conncetion.InsertAsync(person);
+        }
+        
+    }
+}
